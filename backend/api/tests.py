eec5aaa10ddd7e112a05731fd60b2b695ee99425
Dmitriy Nikitenko <jus27rus@gmail.com>
2025-01-17 18:49:51 +1000

    Add tests v2

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,10 +1,12 @@
-# backend/api/tests.py
+"""tests.py."""
 from http import HTTPStatus
 
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
+
     def setUp(self):
         self.guest_client = Client()
 
