704035b1c5892cbb0df5b58e34473af9997bf754
Dmitriy Nikitenko <jus27rus@gmail.com>
2025-01-20 16:07:36 +1000

    Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+if os.getenv('SECRET_KEY'):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    SECRET_KEY = 'secret'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://taski.fun', '84.252.136.128']
+ALLOWED_HOSTS = [os.getenv('URL'), os.getenv('IP'), os.getenv('LOCALHOST')]
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
@@ -51,10 +55,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
